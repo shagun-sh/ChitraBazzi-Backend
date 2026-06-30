199d682fdbd88a0e47b2aa0fb8d8aa3ab9365d90
shagun-sh <shagunsharmadis@gmail.com>
2026-06-30 12:11:31 +0530

    Add root route

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ app.use("/categories", categoryRoutes);
 
 // Server
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "ChitraBazzi Backend Running 🚀"
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server Running on Port ${PORT}`);
