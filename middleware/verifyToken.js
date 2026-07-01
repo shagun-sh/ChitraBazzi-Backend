eca506b32348cb641ded07202cee935b62efb2f6
shagun-sh <shagunsharmadis@gmail.com>
2026-07-01 21:40:26 +0530

    Add JWT debug logs

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,18 +3,29 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  console.log("Authorization Header:", authHeader);
+
   if (!authHeader) {
     return res.status(401).json({ message: "Access Denied" });
   }
 
   const token = authHeader.split(" ")[1];
 
+  console.log("Token:", token);
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("Decoded:", decoded);
+
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Invalid Token" });
+    console.log("JWT VERIFY ERROR:", err);
+
+    return res.status(401).json({
+      message: "Invalid Token",
+    });
   }
 };
 
